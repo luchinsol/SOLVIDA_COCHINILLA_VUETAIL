3114266e5cc2de71b16595a89b1c743153c314f5
luchinsol <luisusca.sol@gmail.com>
2026-05-31 19:19:42 -0500

    fin de usuarios y seguridad

--- src/pages/seguridad/GestionRoles.vue
+++ src/pages/seguridad/GestionRoles.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex flex-col gap-6">
-    <!-- HEADER
     <div class="flex items-center justify-between">
       <div>
         <h2 class="text-[20px] font-bold text-gray-900">Roles del Sistema</h2>
@@ -11,13 +10,14 @@
       </div>
 
       <button
+        @click="showNuevoPermisoModal = true"
         class="px-4 py-2 bg-gray-900 text-white rounded-[8px] text-sm font-medium hover:bg-gray-800 transition-colors shadow-sm flex items-center gap-2"
       >
         <i class="fa-solid fa-plus"></i>
-        Nuevo Rol
+        Nuevo Permiso
       </button>
     </div>
- -->
+
     <!-- MAIN -->
     <div class="flex gap-6 min-h-[700px]">
       <!-- ROLES -->
@@ -68,7 +68,7 @@
 
             <div class="flex items-center gap-2">
               <span
-                class="text-xs font-medium px-2 py-0.5 rounded-full"
+                class="text-xs font-bold px-2 py-0.5 rounded-full"
                 :class="
                   selectedRole?.rol_id === role.rol_id
                     ? 'bg-white border border-blue-400 text-blue-600'
@@ -111,9 +111,10 @@
 
           <div class="flex gap-3">
             <button
-              class="px-4 py-2 bg-gray-900 text-white rounded-[8px] text-sm font-medium hover:bg-gray-800 transition-colors shadow-sm"
+              @click="showGuardarPermisosModal = true"
+              class="px-4 py-2 bg-blue-600 text-white rounded-[8px] text-sm font-medium hover:bg-blue-700 transition-colors shadow-sm"
             >
-              Editar
+              Guardar cambios
             </button>
           </div>
         </div>
@@ -178,49 +179,13 @@
                 </div>
 
                 <!-- ACTIONS -->
-                <div class="flex flex-wrap items-center gap-2">
-                  <button
-                    v-for="action in actions"
-                    :key="action"
-                    class="px-3 py-1.5 rounded-[6px] text-[12px] font-medium border transition-colors flex items-center gap-1.5"
-                    :class="
-                      resource.acciones[action.toLowerCase()]
-                        ? 'bg-blue-50 border-blue-200 text-blue-700'
-                        : 'bg-white border-gray-200 text-gray-400 hover:bg-gray-50 hover:text-gray-600'
-                    "
-                  >
-                    {{ action }}
-                  </button>
-                </div>
-                <!-- SEGMENTED -->
-                <div class="inline-flex bg-gray-100/80 p-1 rounded-[10px] border border-gray-200">
-                  <!-- VALORADO -->
-                  <button
-                    class="px-4 py-2 text-[12px] font-semibold rounded-[8px] flex items-center gap-2 transition-all"
-                    :class="
-                      resource.scope === 'valorado'
-                        ? 'bg-white text-green-700 shadow-sm border border-gray-200'
-                        : 'text-gray-500 hover:text-gray-700'
-                    "
-                    @click="resource.scope = 'valorado'"
-                  >
-                    <i class="fa-solid fa-sack-dollar text-[11px]"></i>
-                    Valorado
-                  </button>
-
-                  <!-- NO VALORADO -->
-                  <button
-                    class="px-4 py-2 text-[12px] font-medium rounded-[8px] flex items-center gap-2 transition-all"
-                    :class="
-                      resource.scope === 'no-valorado'
-                        ? 'bg-white text-gray-700 shadow-sm border border-gray-200'
-                        : 'text-gray-500 hover:text-gray-700'
-                    "
-                    @click="resource.scope = 'no-valorado'"
-                  >
-                    <i class="fa-solid fa-ban text-[11px]"></i>
-                    No valorado
-                  </button>
+                <div class="grid grid-cols-4 gap-3 w-full">
+                  <PermissionSelector
+                    v-for="(permission, accionNombre) in resource.acciones"
+                    :key="accionNombre"
+                    :label="accionNombre"
+                    :permission="permission"
+                  />
                 </div>
               </div>
             </div>
@@ -229,17 +194,227 @@
       </div>
     </div>
   </div>
+
+  <div
+    v-if="showGuardarPermisosModal"
+    class="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+  >
+    <div class="bg-white p-6 rounded-xl w-[500px] shadow-lg">
+      <h2 class="text-xl font-bold mb-4">Guardar cambios de permisos</h2>
+
+      <p class="text-sm text-gray-600 mb-6">
+        Se actualizarán los permisos modificados para el rol
+        <span class="font-semibold"> {{ selectedRole?.nombre }} </span>.
+      </p>
+
+      <div class="flex justify-end gap-3">
+        <button
+          @click="showGuardarPermisosModal = false"
+          class="px-4 py-2 border border-gray-300 rounded-lg"
+        >
+          Cancelar
+        </button>
+
+        <button
+          @click="confirmarGuardarPermisos"
+          class="px-4 py-2 bg-[#1E3A5F] text-white rounded-lg"
+        >
+          Guardar
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- SHOW MODAL NUEVO PERMISO  -->
+  <div
+    v-if="showNuevoPermisoModal"
+    class="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+  >
+    <div class="bg-white w-[900px] rounded-xl shadow-xl overflow-hidden">
+      <!-- HEADER -->
+      <div class="flex items-center justify-between px-6 py-4 border-b bg-blue-900">
+        <div class="flex items-center gap-3">
+          <div
+            class="w-9 h-9 rounded-xl flex items-center justify-center bg-white/10 border border-white/20"
+          >
+            <i class="fa-solid fa-plus text-white text-sm"></i>
+          </div>
+
+          <div>
+            <h2 class="text-sm font-bold text-white">Agregar Nuevo Permiso</h2>
+
+            <p class="text-[11px] text-indigo-200">Configura módulo, recurso y acciones</p>
+          </div>
+        </div>
+
+        <button
+          @click="$emit('close')"
+          class="w-8 h-8 rounded-lg bg-white/10 hover:bg-white/20 text-white"
+        >
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
+
+      <!-- INFO -->
+      <div class="flex items-center gap-2 px-6 py-3 bg-indigo-50 border-b text-xs text-blue-900">
+        <i class="fa-solid fa-circle-info"></i>
+
+        <span> Selecciona un módulo para habilitar el recurso. </span>
+      </div>
+
+      <!-- BODY -->
+      <div class="p-6 space-y-6">
+        <!-- SELECTORES -->
+        <div class="grid grid-cols-2 gap-4">
+          <!-- MODULO -->
+          <div>
+            <label class="block text-xs font-semibold mb-2"> Módulo </label>
+
+            <select
+              v-model="form.modulo_id"
+              @change="cargarRecursos(form.modulo_id)"
+              class="w-full border rounded-lg px-3 py-2"
+            >
+              <option value="">Seleccionar módulo</option>
+
+              <option v-for="modulo in modulos" :key="modulo.modulo_id" :value="modulo.modulo_id">
+                {{ modulo.nombre }}
+              </option>
+            </select>
+          </div>
+
+          <!-- RECURSO -->
+          <div>
+            <label class="block text-xs font-semibold mb-2"> Recurso </label>
+
+            <select v-model="form.recurso" class="w-full border rounded-lg px-3 py-2">
+              <option value="">Seleccionar recurso</option>
+
+              <option v-for="recurso in recursos" :key="recurso.recurso" :value="recurso.recurso">
+                {{ recurso.recurso_label }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <!-- ACCIONES -->
+        <div class="border rounded-xl overflow-hidden">
+          <div class="grid grid-cols-4 bg-blue-900 text-white px-4 py-3 text-xs font-semibold">
+            <div></div>
+            <div>Acción</div>
+            <div>Descripción</div>
+            <div class="text-center">Alcance</div>
+          </div>
+
+          <div
+            v-if="recursoSeleccionado"
+            v-for="(accionData, accionNombre) in recursoSeleccionado.acciones"
+            :key="accionNombre"
+            class="grid grid-cols-4 items-center px-4 py-3 border-t"
+          >
+            <div class="flex justify-center">
+              <input type="checkbox" v-model="accionData.activo" />
+            </div>
+
+            <div class="font-medium capitalize">
+              {{ accionNombre }}
+            </div>
+
+            <div class="text-xs text-gray-500">
+              {{
+                accionData.opciones?.valorado?.descripcion ||
+                accionData.opciones?.no_valorado?.descripcion ||
+                accionData.opciones?.on?.descripcion ||
+                '-'
+              }}
+            </div>
+
+            <div class="flex justify-center gap-2">
+              <button
+                v-if="accionData.opciones?.valorado?.permiso_id"
+                @click="accionData.alcanceSeleccionado = 'valorado'"
+                :class="
+                  accionData.alcanceSeleccionado === 'valorado' ? 'bg-blue-600 text-white' : ''
+                "
+                class="px-2 py-1 text-xs border rounded"
+              >
+                V
+              </button>
+
+              <button
+                v-if="accionData.opciones?.no_valorado?.permiso_id"
+                @click="accionData.alcanceSeleccionado = 'no_valorado'"
+                :class="
+                  accionData.alcanceSeleccionado === 'no_valorado' ? 'bg-blue-600 text-white' : ''
+                "
+                class="px-2 py-1 text-xs border rounded"
+              >
+                NV
+              </button>
+              <span v-if="accionData.tipo_selector === 'booleano'" class="text-xs text-slate-500">
+                Booleano
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- FOOTER -->
+      <div class="flex justify-end gap-3 p-4 border-t bg-gray-50">
+        <button @click="showNuevoPermisoModal = false" class="px-4 py-2 border rounded-lg">
+          Cancelar
+        </button>
+
+        <button @click="guardarPermiso" class="px-4 py-2 bg-blue-900 text-white rounded-lg">
+          Guardar Permiso
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref, computed } from 'vue'
 import axios from 'axios'
+import PermissionSelector from './PermisoSelector/PermissionSelector.vue'
 
 const roles = ref([])
 const searchRole = ref('')
 const rolePermissions = ref([])
-
+const originalPermissions = ref([])
 const selectedRole = ref(null)
+const showGuardarPermisosModal = ref(false)
+// modal nuevo permiso
+const showNuevoPermisoModal = ref(false)
+// modales
+const modulos = ref([])
+const recursos = ref([])
+
+const form = ref({
+  modulo_id: '',
+  recurso: '',
+})
+
+const postPermiso = async () => {
+  try {
+    //await axios.post(...)
+
+    showNuevoPermisoModal.value = false
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const acciones = ref([
+  {
+    codigo: 'ver',
+    nombre: 'Ver',
+    descripcion: 'Visualizar registros',
+    activo: false,
+    alcance: null,
+  },
+])
+
 const filteredRoles = computed(() => {
   if (!searchRole.value) {
     return roles.value
@@ -280,22 +455,245 @@ const getListRoles = async () => {
 const getRolePermissions = async (rolId) => {
   try {
     const baseUrl = import.meta.env.VITE_API_URL
-
+    const token = localStorage.getItem('token')
     const response = await axios.get(`${baseUrl}/rol-permisos/vista?rol_id=${rolId}`, {
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_TOKEN_LOGIN}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     })
 
     rolePermissions.value = response.data.modulos
 
     console.log('Permisos del rol:', rolePermissions.value)
+    originalPermissions.value = JSON.parse(JSON.stringify(response.data.modulos))
   } catch (error) {
     console.error('Error cargando permisos:', error)
   }
 }
 
+const obtenerCambios = () => {
+  const cambios = []
+
+  rolePermissions.value.forEach((modulo, moduloIndex) => {
+    modulo.recursos.forEach((recurso, recursoIndex) => {
+      Object.entries(recurso.acciones).forEach(([accionNombre, accion]) => {
+        const original =
+          originalPermissions.value[moduloIndex].recursos[recursoIndex].acciones[accionNombre]
+
+        if (accion.tipo_selector === 'booleano') {
+          if (accion.activo !== original.activo) {
+            cambios.push({
+              rol_id: selectedRole.value.rol_id,
+              recurso: recurso.recurso,
+              accion: accionNombre,
+              alcance: null,
+              activo: accion.activo,
+            })
+          }
+        }
+
+        if (accion.tipo_selector === 'alcance') {
+          if (accion.opciones.valorado.activo !== original.opciones.valorado.activo) {
+            cambios.push({
+              rol_id: selectedRole.value.rol_id,
+              recurso: recurso.recurso,
+              accion: accionNombre,
+              alcance: 'valorado',
+              activo: accion.opciones.valorado.activo,
+            })
+          }
+
+          if (accion.opciones.no_valorado.activo !== original.opciones.no_valorado.activo) {
+            cambios.push({
+              rol_id: selectedRole.value.rol_id,
+              recurso: recurso.recurso,
+              accion: accionNombre,
+              alcance: 'no_valorado',
+              activo: accion.opciones.no_valorado.activo,
+            })
+          }
+        }
+      })
+    })
+  })
+
+  return cambios
+}
+
+const construirCambios = () => {
+  const cambios = []
+
+  rolePermissions.value.forEach((modulo) => {
+    modulo.recursos.forEach((recurso) => {
+      Object.entries(recurso.acciones).forEach(([accionNombre, accion]) => {
+        if (accion.tipo_selector === 'alcance') {
+          cambios.push({
+            rol_id: selectedRole.value.rol_id,
+            recurso: recurso.recurso,
+            accion: accionNombre,
+            alcance: 'valorado',
+            activo: accion.opciones.valorado.activo,
+          })
+
+          cambios.push({
+            rol_id: selectedRole.value.rol_id,
+            recurso: recurso.recurso,
+            accion: accionNombre,
+            alcance: 'no_valorado',
+            activo: accion.opciones.no_valorado.activo,
+          })
+        } else {
+          cambios.push({
+            rol_id: selectedRole.value.rol_id,
+            recurso: recurso.recurso,
+            accion: accionNombre,
+            alcance: null,
+            activo: accion.activo,
+          })
+        }
+      })
+    })
+  })
+
+  return cambios
+}
+const guardarCambios = async () => {
+  try {
+    //   const cambios = construirCambios()
+    const cambios = obtenerCambios()
+    const token = localStorage.getItem('token')
+
+    if (!cambios.length) {
+      alert('No hay cambios para guardar')
+      return
+    }
+    console.log('Payload:', cambios)
+
+    const baseUrl = import.meta.env.VITE_API_URL
+    /*
+    await Promise.all(
+      cambios.map((item) =>
+        axios.patch(`${baseUrl}/rol-permisos`, item, {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TOKEN_LOGIN}`,
+          },
+        }),
+      ),
+    )*/
+    for (const item of cambios) {
+      try {
+        console.log('ENVIANDO:', item)
+
+        const response = await axios.patch(`${baseUrl}/rol-permisos`, item, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        })
+
+        console.log('OK:', response.data)
+      } catch (error) {
+        console.log('FALLO:', item)
+        console.log(error.response?.data)
+      }
+    }
+    originalPermissions.value = JSON.parse(JSON.stringify(rolePermissions.value))
+    alert('Permisos actualizados correctamente')
+  } catch (error) {
+    console.error('ERROR COMPLETO:', error.response?.data)
+    console.error(error)
+  }
+}
+const confirmarGuardarPermisos = async () => {
+  await guardarCambios()
+
+  showGuardarPermisosModal.value = false
+}
+
+const getModulos = async () => {
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+    const response = await axios.get(`${baseUrl}/modulos`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+    modulos.value = response.data
+  } catch (error) {
+    console.error('ERROR:', error)
+  }
+}
+
+const cargarRecursos = async (moduloId) => {
+  if (!moduloId) return
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+    const response = await axios.get(`${baseUrl}/permisos/catalogo?modulo_id=${moduloId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+    recursos.value = response.data[0].recursos
+  } catch (error) {
+    console.error('ERROR:', error)
+  }
+}
+
+const recursoSeleccionado = computed(() => {
+  return recursos.value.find((r) => r.recurso === form.value.recurso)
+})
+
+const guardarPermiso = async () => {
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    const moduloSeleccionado = modulos.value.find((m) => m.modulo_id == form.value.modulo_id)
+
+    const permisos = []
+
+    Object.entries(recursoSeleccionado.value.acciones).forEach(([accionNombre, accionData]) => {
+      if (accionData.activo) {
+        permisos.push({
+          modulo: moduloSeleccionado.nombre,
+          recurso: form.value.recurso,
+          accion: accionNombre,
+          alcance:
+            accionData.tipo_selector === 'alcance' ? accionData.alcanceSeleccionado || null : null,
+          descripcion:
+            accionData.opciones?.valorado?.descripcion ||
+            accionData.opciones?.no_valorado?.descripcion ||
+            accionData.opciones?.on?.descripcion ||
+            '',
+        })
+      }
+    })
+
+    console.log('Payload:', permisos)
+    console.log(localStorage.getItem('token'))
+    await Promise.all(
+      permisos.map((item) =>
+        axios.post(`${baseUrl}/permisos`, item, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }),
+      ),
+    )
+    console.log(item)
+    console.log({
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    })
+    showNuevoPermisoModal.value = false
+
+    alert('Permisos creados correctamente')
+  } catch (error) {
+    console.log(error.response?.status)
+    console.log(error.response?.data)
+  }
+}
+
 onMounted(() => {
   getListRoles()
+  getModulos()
+  //  cargarRecursos()
 })
 </script>
--- src/pages/seguridad/SecurityView.vue
+++ src/pages/seguridad/SecurityView.vue
@@ -15,7 +15,10 @@
           Exportar Auditoría
         </button>
 
-        <button class="px-4 py-2 bg-primary text-secondary rounded-xl text-sm font-bold">
+        <button
+          @click="showNuevoUsuarioModal = true"
+          class="px-4 py-2 bg-primary text-secondary rounded-xl text-sm font-bold"
+        >
           Nuevo Usuario
         </button>
       </div>
@@ -68,11 +71,106 @@
       <GestionRoles />
     </template>
   </div>
+
+  <!-- MODAL NUEVO USUARIO -->
+  <div
+    v-if="showNuevoUsuarioModal"
+    class="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+  >
+    <div class="bg-white w-[800px] rounded-2xl shadow-2xl overflow-hidden">
+      <!-- HEADER -->
+      <div class="bg-slate-900 px-6 py-4 flex justify-between items-center">
+        <div>
+          <h2 class="text-white text-lg font-bold">Nuevo Usuario</h2>
+
+          <p class="text-slate-300 text-xs">Registro de usuarios del sistema</p>
+        </div>
+
+        <button @click="showNuevoUsuarioModal = false" class="text-white text-xl">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
+
+      <!-- BODY -->
+      <div class="p-6">
+        <div class="grid grid-cols-2 gap-4">
+          <div>
+            <label class="block text-sm font-semibold mb-2"> Nombres </label>
+
+            <input v-model="formUsuario.nombres" class="w-full border rounded-xl px-3 py-2" />
+          </div>
+
+          <div>
+            <label class="block text-sm font-semibold mb-2"> Apellidos </label>
+
+            <input v-model="formUsuario.apellidos" class="w-full border rounded-xl px-3 py-2" />
+          </div>
+
+          <div>
+            <label class="block text-sm font-semibold mb-2"> Correo </label>
+
+            <input
+              v-model="formUsuario.correo"
+              type="email"
+              class="w-full border rounded-xl px-3 py-2"
+            />
+          </div>
+
+          <div>
+            <label class="block text-sm font-semibold mb-2"> Nickname </label>
+
+            <input v-model="formUsuario.nickname" class="w-full border rounded-xl px-3 py-2" />
+          </div>
+
+          <div>
+            <label class="block text-sm font-semibold mb-2"> DNI </label>
+
+            <input v-model="formUsuario.dni" class="w-full border rounded-xl px-3 py-2" />
+          </div>
+
+          <div>
+            <label class="block text-sm font-semibold mb-2"> Rol </label>
+
+            <select v-model="formUsuario.rol_id" class="w-full border rounded-xl px-3 py-2">
+              <option value="">Seleccionar rol</option>
+
+              <option v-for="rol in roles" :key="rol.rol_id" :value="rol.rol_id">
+                {{ rol.nombre }}
+              </option>
+            </select>
+          </div>
+
+          <div class="col-span-2">
+            <label class="block text-sm font-semibold mb-2"> Departamento </label>
+
+            <select v-model="formUsuario.departamento" class="w-full border rounded-xl px-3 py-2">
+              <option value="">Seleccionar departamento</option>
+
+              <option v-for="item in departamentos" :key="item.modulo_id" :value="item.nombre">
+                {{ item.nombre }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- FOOTER -->
+      <div class="border-t bg-slate-50 px-6 py-4 flex justify-end gap-3">
+        <button @click="showNuevoUsuarioModal = false" class="px-4 py-2 border rounded-xl">
+          Cancelar
+        </button>
+
+        <button @click="guardarUsuario" class="px-4 py-2 bg-slate-900 text-white rounded-xl">
+          Guardar Usuario
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
+import { ref, onMounted, computed } from 'vue'
+import axios from 'axios'
 import UserTable from '@/pages/seguridad/UserTable.vue'
 import UserEditPanel from '@/pages/seguridad/UserEditPanel.vue'
 import GestionRoles from '@/pages/seguridad/GestionRoles.vue'
@@ -81,6 +179,88 @@ const activeTab = ref('usuarios')
 
 const userTableRef = ref(null)
 const selectedUser = ref(null)
+const roles = ref([])
+const showNuevoUsuarioModal = ref(false)
+
+const formUsuario = ref({
+  nombres: '',
+  apellidos: '',
+  rol_id: '',
+  correo: '',
+  nickname: '',
+  dni: '',
+  departamento: '',
+})
+const cargarRoles = async () => {
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    const response = await axios.get(`${baseUrl}/roles`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+
+    roles.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+const cargarDepartamentos = async () => {
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    const response = await axios.get(`${baseUrl}/modulos`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+
+    departamentos.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+const guardarUsuario = async () => {
+  try {
+    const baseUrl = import.meta.env.VITE_API_URL
+
+    await axios.post(
+      `${baseUrl}/usuarios`,
+      {
+        nombres: formUsuario.value.nombres,
+        apellidos: formUsuario.value.apellidos,
+        rol_id: Number(formUsuario.value.rol_id),
+        correo: formUsuario.value.correo,
+        nickname: formUsuario.value.nickname,
+        dni: Number(formUsuario.value.dni),
+        departamento: formUsuario.value.departamento,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      },
+    )
+
+    showNuevoUsuarioModal.value = false
+
+    formUsuario.value = {
+      nombres: '',
+      apellidos: '',
+      rol_id: '',
+      correo: '',
+      nickname: '',
+      dni: '',
+      departamento: '',
+    }
+
+    alert('Usuario creado correctamente')
+  } catch (error) {
+    console.error(error.response?.data)
+  }
+}
+const departamentos = ref([])
 
 const onEditUser = (user) => {
   selectedUser.value = user
@@ -89,4 +269,9 @@ const onEditUser = (user) => {
 const onUserSaved = async () => {
   await userTableRef.value.getUsers()
 }
+
+onMounted(() => {
+  cargarDepartamentos()
+  cargarRoles()
+})
 </script>
